38d9ef11ff0b35b0834cc146c424c83c987a8eba
Mike <michael.ries@gmail.com>
2017-11-11 11:51:08 -0600

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,10 +1,33 @@
-describe('ContactController', function () {
+describe("ContactController", function() {
     var $controller;
 
-    beforeEach(module('app'));
+    beforeEach(module("app"));
 
-    beforeEach(inject(function (_$controller_) {
-        $controller = _$controller_;
-    }));
+    beforeEach(
+        inject(function(_$controller_) {
+            $controller = _$controller_;
+        })
+    );
 
+    it("should filter list for 71 females", function() {
+        var $scope = {};
+        $controller("ContactController as vm", { $scope: $scope });
+
+        // $scope.vm.search = "F";
+
+        // $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toEqual(71);
+    });
+
+    it("should find 8 people from Manchester", function() {
+        var $scope = {};
+        $controller("ContactController as vm", { $scope: $scope });
+
+        $scope.vm.search = { location: { city: "Manchester" } };
+
+        $scope.vm.changeFilter();
+
+        expect($scope.vm.filteredList.length).toEqual(2);
+    });
 });
