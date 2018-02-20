18bcf4cd3390b632749494b3eb709811639059be
r0tiart <antley.chu@gmail.com>
2018-02-20 14:51:31 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -6,5 +6,27 @@ describe('ContactController', function () {
     beforeEach(inject(function (_$controller_) {
         $controller = _$controller_;
     }));
+    
 
+    it('should be able to filter by gender', function () {
+	    var $scope = {};
+	    $controller('ContactController as vm', {$scope: $scope});
+	 
+	    $scope.vm.search = 'female';
+	 
+	    $scope.vm.changeFilter();
+	 
+	    expect($scope.vm.filteredList.length).toBe(71);
+	});
+
+    it('should be able to filter by geography', function () {
+	    var $scope = {};
+	    $controller('ContactController as vm', {$scope: $scope});
+	 
+	    $scope.vm.search = 'Manchester';
+	 
+	    $scope.vm.changeFilter();
+	 
+	    expect($scope.vm.filteredList.length).toBe(8);
+	});
 });
