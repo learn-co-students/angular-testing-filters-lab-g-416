e24feebe671feb78e7c1a2d72942f234310a4f1a
ferrisbueller66 <tmtarpinian@gmail.com>
2021-01-27 13:40:46 -0500

    Done.

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -7,4 +7,20 @@ describe('ContactController', function () {
         $controller = _$controller_;
     }));
 
+    it('should filter all female contacts correctly', function () {
+        var $scope = {};
+        $controller('ContactController as ctrl', {$scope: $scope});
+        $scope.ctrl.search = 'female'
+        $scope.ctrl.changeFilter()
+        expect($scope.ctrl.filteredList.length).toEqual(71);
+    });
+
+    it('should filter all contacts geographically', function () {
+        var $scope = {};
+        $controller('ContactController as ctrl', {$scope: $scope});
+        $scope.ctrl.search = 'Manchester'
+        $scope.ctrl.changeFilter()
+        expect($scope.ctrl.filteredList.length).toEqual(8);
+    });
+
 });
